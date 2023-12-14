c2258a31a0808220724f9691fbbbda09ceef7dc2
dev1 <dev1@email.com>
2023-12-14 13:07:35 +0000

    mongodb

---
+++ catalogue.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+DATE=$(date +%F)
+
+LOG="/tmp/$0-$DATE.log"
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+
+VALIDATE(){
+if	[ $1 -ne 0 ];then
+	echo -e "$2.....${R} FAILURE ${N}" 
+	exit 1
+		else
+				echo -e "$2... ${G} SUCCESS ${N}" 
+
+fi
+}
+
+if [ $USERID -ne 0 ]; then
+	echo -e "${R} if you need to be root user excute this script ${N}"
+	exit 1
+    else 
+    echo -e "$Y you are root user $N"
+fi
+
+
+dnf module disable nodejs -y &>> $LOG
+VALIDATE $? "disable nodejs"
+
+
+dnf module enable nodejs:18 -y &>> $LOG
+VALIDATE $? "enable nodejs 18"
+
+dnf install nodejs -y &>> $LOG
+VALIDATE $? "install nodejs"
+
+useradd roboshop &>> $LOG
+VALIDATE $? "adding roboshop user"
+
+mkdir /app &>> $LOG
+VALIDATE $? "crete dirctory app"
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOG
+VALIDATE $? "application download is"
+
+cd /app &>> $LOG
+VALIDATE $? "change app"
+
+unzip /tmp/catalogue.zip &>> $LOG
+VALIDATE $? "unzip file is"
+
+npm install &>> $LOG
+VALIDATE $? "npm install is"
+
+cp catalogue.service /etc/systemd/system/catalogue.service &>> $LOG
+VALIDATE $? "copied catalogue service"
+
+systemctl daemon-reload &>> $LOG
+VALIDATE $? "deamon reload"
+
+systemctl enable catalogue &>> $LOG
+VALIDATE $? "catalogue enabled"
+
+systemctl start catalogue &>> $LOG
+VALIDATE $? "catalogue start"
+
+
+
+
+
+
+
+
+
+
+
