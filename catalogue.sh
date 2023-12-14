c40926bb24d8e8253c627358b25e96700f4aebeb
dev1 <dev1@email.com>
2023-12-14 14:29:56 +0000

    mongodb

--- catalogue.sh
+++ catalogue.sh
@@ -6,6 +6,8 @@ DATE=$(date +%F)
 
 LOG="/tmp/$0-$DATE.log"
 
+MONGODB_HOST=mongodb.daws76.online
+
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
@@ -38,12 +40,19 @@ dnf module enable nodejs:18 -y &>> $LOG
 VALIDATE $? "enable nodejs 18"
 
 dnf install nodejs -y &>> $LOG
-VALIDATE $? "install nodejs"
+VALIDATE $? "install nodejs:18"
 
-useradd roboshop &>> $LOG
+id roboshop
+if [ $? -ne 0 ];then
+    useradd roboshop 
+    VALIDATE $?"robosop user creation"
+    else
+        echo -e "roboshop user aleready exists $Y Skipping $N"
+fi
+
 VALIDATE $? "adding roboshop user"
 
-mkdir /app &>> $LOG
+mkdir -p /app &>> $LOG
 VALIDATE $? "crete dirctory app"
 
 curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOG
@@ -52,7 +61,7 @@ VALIDATE $? "application download is"
 cd /app &>> $LOG
 VALIDATE $? "change app"
 
-unzip /tmp/catalogue.zip &>> $LOG
+unzip -o /tmp/catalogue.zip &>> $LOG
 VALIDATE $? "unzip file is"
 
 npm install &>> $LOG
@@ -76,7 +85,7 @@ VALIDATE $? "copied mongodb repo"
 dnf install mongodb-org-shell -y &>> $LOG
 VALIDATE $? "install mongodb"
 
-mongo --host mongodb.daws76.online </app/schema/catalogue.js &>> $LOG
+mongo --host $MONGODB_HOST </app/schema/catalogue.js &>> $LOG
 VALIDATE $? "loading catalogue data into mangodb"
 
 
